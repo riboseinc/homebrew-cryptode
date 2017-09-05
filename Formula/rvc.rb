2a27589371310c13b676d68a68ddb32d9c429fe0
Ronald Tse <ronald.tse@ribose.com>
2017-09-06 07:51:26 +0800

    Fix rvd.json and caveats permissions

--- Formula/rvc.rb
+++ Formula/rvc.rb
@@ -28,16 +28,17 @@ class Rvc < Formula
     system "./configure", "--prefix=#{prefix}"
     system "make", "install"
 
-    # We're creating the plist manually now
     # prefix.install "plist/com.ribose.rvd.plist"
     # (prefix+"com.ribose.rvd.plist").chmod 0644
-
-    # Replace user 501 with current installing user
     inreplace "conf/rvd.json", "501", `id -u`
 		(etc/"rvd").install "conf/rvd.json"
   end
 
   plist_options startup: false
+
+  def target_prefix
+    "/opt/rvc"
+  end
 
   def plist; <<-EOS.undent
 <?xml version="1.0" encoding="UTF-8"?>
@@ -68,45 +69,39 @@ class Rvc < Formula
     EOS
   end
 
-  # This is where we want the rvc binary to reside
-  def target_prefix
-    "/opt/rvc"
-  end
-
-  # This is where we want the openvpn binary to reside
 	def opt_openvpn
 		"/opt/openvpn"
 	end
 
-  # Override plist_name to com.ribose namespace
 	def plist_name
 		"com.ribose.rvd"
 	end
 
+  #sudo install -m 500 -g wheel -o root #{prefix}/com.ribose.rvd.plist /Library/LaunchDaemons
   def caveats; <<-EOS.undent
     rvc requires to be installed in `/opt` and requires root privileges to start
     run:
-      sudo mkdir -m 500 -p #{target_prefix}/bin #{target_prefix}/etc/vpn.d #{opt_openvpn}/sbin
+      sudo mkdir -m 700 -p #{target_prefix}/bin #{opt_openvpn}/sbin
+      sudo mkdir -m 700 -p #{target_prefix}/etc/vpn.d
       sudo chown -R root:wheel #{target_prefix} #{opt_openvpn}/
       sudo install -m 500 -g wheel -o root #{bin/"rvc"} #{bin/"rvd"} #{target_prefix}/bin
-      sudo install -m 500 -g wheel -o root #{etc/"rvd.json"} #{target_prefix}/etc
-      #sudo install -m 500 -g wheel -o root #{prefix/(plist_name+".plist")} /Library/LaunchDaemons
+      sudo install -m 600 -g wheel -o root #{etc/"rvd.json"} #{target_prefix}/etc
       sudo install -m 500 -g wheel -o root #{Formula["openvpn"].opt_sbin}/openvpn #{opt_openvpn}/sbin
 
     To load #{name} at startup, activate the included LaunchDaemon:
 
     If this is your first install, automatically load on startup with:
-      sudo install -m 500 -g wheel -o root #{prefix}/com.ribose.rvd.plist /Library/LaunchDaemons
-      sudo launchctl load -w /Library/LaunchDaemons/com.ribose.rvd.plist
+      sudo install -m 600 -g wheel -o root #{prefix/(plist_name+".plist")} /Library/LaunchDaemons
+      sudo launchctl load -w /Library/LaunchDaemons/#{plist_name}.plist
 
     If this is an upgrade and you already have the plist loaded:
-      sudo launchctl unload -w /Library/LaunchDaemons/com.ribose.rvd.plist
-      sudo install -m 500 -g wheel -o root #{prefix}/com.ribose.rvd.plist /Library/LaunchDaemons
-      sudo launchctl load -w /Library/LaunchDaemons/com.ribose.rvd.plist
+      sudo launchctl unload /Library/LaunchDaemons/#{plist_name}.plist
+      sudo install -m 600 -g wheel -o root #{prefix/(plist_name+".plist")} /Library/LaunchDaemons
+      sudo launchctl load -w /Library/LaunchDaemons/#{plist_name}.plist
     EOS
   end
 
   test do
     system "rvc", "--version"
   end
-end
+e
